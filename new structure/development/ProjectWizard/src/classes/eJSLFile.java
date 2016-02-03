0f778a60bae18bbff0753a46edcd4b9ffd7d7b93
Max Steinwachs <Max Steinwachs>
2016-02-03 15:22:20 +0100

    add icon to new eJSL File

--- new structure/development/ProjectWizard/src/classes/eJSLFile.java
+++ new structure/development/ProjectWizard/src/classes/eJSLFile.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.IconLoader;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,6 +14,11 @@ import java.io.IOException;
  * Created by Leon on 27.01.16.
  */
 public class eJSLFile extends AnAction{
+
+    public eJSLFile(){
+        super("eJSLFile","Creates a new eJSL File", IconLoader.getIcon("/resources/icons/eJSL.PNG"));
+    }
+
 
     @Override
     public void actionPerformed(AnActionEvent event) {
--- new structure/development/ProjectWizard/src/classes/eJSLModuleType.java
+++ new structure/development/ProjectWizard/src/classes/eJSLModuleType.java
@@ -4,10 +4,11 @@ import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider;
-import com.intellij.util.PathUtil;
+import com.intellij.openapi.util.IconLoader;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+
 
 /**
  * Created by Leon on 21.01.16.
@@ -44,16 +45,12 @@ public class eJSLModuleType extends ModuleType<eJSLModuleBuilder> {
 
     @Override
     public Icon getBigIcon() {
-        ImageIcon icon = new ImageIcon(PathUtil.getJarPathForClass(getClass())+ "/resources/icons/eJSL.png", "hi");
-        return icon;
-        //AllIcons.General.Information;
+        return IconLoader.getIcon("/resources/icons/eJSL.PNG");
     }
 
     @Override
     public Icon getNodeIcon(@Deprecated boolean b) {
-        ImageIcon icon =  new ImageIcon(PathUtil.getJarPathForClass(getClass())+ "/resources/icons/eJSL.png", "hi");
-        return icon;
-         // AllIcons.General.Information;
+        return IconLoader.getIcon("/resources/icons/eJSL.PNG");
     }
 
 
