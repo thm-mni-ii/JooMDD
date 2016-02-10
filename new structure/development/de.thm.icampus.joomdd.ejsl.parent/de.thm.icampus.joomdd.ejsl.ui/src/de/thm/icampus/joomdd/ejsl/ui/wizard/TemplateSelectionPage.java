a860393315a75b253019d52ae0d0f28618223cfd
Ewald Bayer <ewald.bayer@mni.thm.de>
2016-02-10 15:29:41 +0100

    Bug fixes and improvements Fixed refresh bug: Project is refreshed automatically after creation now. Fixed template name bug: The way templates are loaded entirely changed. New: Templates are now managed inside a XML file in the same folder. It is validated by the XSD file in the same folder. New: Wizard shows the description of selected template (if any) and a preview of files content.
    Known bugs:
    When resizing the wizard window, description and preview labels could be
    resized strangely.
    Preview of templates contain actual file content. "preview" tag inside
    the xml is ignored so far.

--- new structure/development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.ui/src/de/thm/icampus/joomdd/ejsl/ui/wizard/TemplateSelectionPage.java
+++ new structure/development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.ui/src/de/thm/icampus/joomdd/ejsl/ui/wizard/TemplateSelectionPage.java
@@ -2,6 +2,8 @@ package de.thm.icampus.joomdd.ejsl.ui.wizard;
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 import org.eclipse.core.runtime.FileLocator;
@@ -9,15 +11,23 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 
-public class TemplateSelectionPage extends WizardPage {
+public class TemplateSelectionPage extends WizardPage implements SelectionListener {
 
 	private Composite container;
 	private Button[] buttons;
-	private File[] templateFiles;
+	private Template[] templates;
+	private Label template_description;
+	private Label template_content;
+	private String templateDirectory;
 	
 	protected TemplateSelectionPage(String pageName) {
 		super(pageName);
@@ -26,32 +36,93 @@ public class TemplateSelectionPage extends WizardPage {
 	@Override
 	public void createControl(Composite parent) {
 		container = new Composite(parent, SWT.NONE);
-		container.setLayout(new GridLayout(1, false));
+		container.setLayout(new GridLayout(2, false));
+		
+		Group g1 = new Group(container, SWT.SHADOW_NONE); 
+		g1.setLayout(new GridLayout(1, false));
+		g1.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		g1.setText("Templates");
+		
+		Group g2 = new Group(container, SWT.SHADOW_NONE); 
+		g2.setLayout(new GridLayout(1, false));
+		g2.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		g2.setText("Selected");
 		try {
-			String templateDirectory = FileLocator.resolve(FileLocator.find(Platform.getBundle("de.thm.icampus.joomdd.ejsl.ui"), new Path("templates"), null)).getFile();
-			templateFiles = new File(templateDirectory).listFiles();
-			buttons = new Button[templateFiles.length];
+			
+			templateDirectory = FileLocator.resolve(FileLocator.find(Platform.getBundle("de.thm.icampus.joomdd.ejsl.ui"), new Path("templates"), null)).getFile();
+			TemplateList list = TemplateXMLLoader.loadTemplates(templateDirectory+"\\TemplateList.xml", templateDirectory+"\\TemplateList.xsd");
+			Object[] temp_templates = list.getTemplates();
+			templates = new Template[temp_templates.length];
+			buttons = new Button[temp_templates.length];
 			for (int i = 0; i < buttons.length; i++) {
-				buttons[i] = new Button(container, SWT.RADIO);
-				buttons[i].setText(templateFiles[i].getName().split("\\.")[0]);
+				templates[i] = ((Template)temp_templates[i]);
+				buttons[i] = new Button(g1, SWT.RADIO);
+				buttons[i].setText(templates[i].getName());
+				buttons[i].addSelectionListener(this);
 			}
+			template_description = new Label(g2, SWT.NONE);
+			template_content = new Label(g2, SWT.NONE);
+
+			template_description.setLayoutData(new GridData(SWT.FILL, SWT.DEFAULT, true, false));
+			template_content.setLayoutData(new GridData(SWT.FILL, SWT.DEFAULT, true, false));
+			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		setControl(container);
-		if(buttons.length > 0)
+		
+		if(buttons.length > 0){
 			buttons[0].setSelection(true);
+			template_description.setText(templates[0].getDescription());
+			setContentLabel(0);
+		}
 		setPageComplete(true);
 	}
 
 	public File getSelectedTemplate(){
 		for (int i = 0; i < buttons.length; i++) {
 			if(buttons[i].getSelection()){
-				return templateFiles[i];
+				return new File(templateDirectory + "\\" + templates[i].getSrc().toString());
 			}
 		}
 		return null;
 	}
+
+	private void setContentLabel(int index){
+		try {
+			int c;
+			String s = "";
+			FileReader fr = new FileReader(new File(templateDirectory + "\\" + templates[index].getSrc()));
+			while((c=fr.read())!=-1){
+				s += (char)c;
+			}
+			fr.close();
+			template_content.setText(s);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void widgetSelected(SelectionEvent e) {
+		for (int i = 0; i < buttons.length; i++) {
+			if(e.getSource() == buttons[i]){
+				if(templates[i].getDescription() == null){
+					template_description.setText("");
+				}else{
+					template_description.setText(templates[i].getDescription());
+				}
+				setContentLabel(i);
+				template_description.pack();
+				template_content.pack();
+				return;
+			}
+		}
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {}
 	
 }
