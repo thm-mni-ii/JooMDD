fb2c49aeadf3abba0cef345daa5c848108755148
Dennis Priefer <dennis.priefer@mni.thm.de>
2016-03-07 15:58:07 +0100

    fix untracked files

--- development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl/xtend-gen/de/thm/icampus/joomdd/ejsl/generator/pi/ExtendedExtension/ExtendedExtensionPackage.java
+++
@@ -1,7 +0,0 @@
-package de.thm.icampus.joomdd.ejsl.generator.pi.ExtendedExtension;
-
-import de.thm.icampus.joomdd.ejsl.eJSL.ExtensionPackage;
-
-@SuppressWarnings("all")
-public interface ExtendedExtensionPackage extends ExtensionPackage {
-}
--- development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl/xtend-gen/de/thm/icampus/joomdd/ejsl/generator/pi/ExtendedExtension/ExtendedPageReference.java
+++
@@ -1,9 +0,0 @@
-package de.thm.icampus.joomdd.ejsl.generator.pi.ExtendedExtension;
-
-import de.thm.icampus.joomdd.ejsl.eJSL.PageReference;
-import de.thm.icampus.joomdd.ejsl.generator.pi.ExtendedPage.ExtendedPage;
-
-@SuppressWarnings("all")
-public interface ExtendedPageReference extends PageReference {
-  public abstract ExtendedPage getExtendedPage();
-}
