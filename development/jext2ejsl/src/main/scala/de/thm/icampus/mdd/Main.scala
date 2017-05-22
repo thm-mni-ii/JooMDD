4056e649b640064aa2743f852681a4b6dcaea378
Tobias Viehmann <tobias.viehmann@mni.thm.de>
2017-05-22 19:00:02 +0200

    Adding new console arguments

--- development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/Main.scala
+++ development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/Main.scala
@@ -1,29 +1,57 @@
 package de.thm.icampus.mdd
 
-import java.io.PrintWriter
-import java.nio.file.Path
-import javax.swing.{JOptionPane, JFileChooser}
+import java.io.{File, PrintWriter}
+import java.nio.file.{Path, Paths}
+import javax.swing.{JFileChooser, JOptionPane}
 import javax.swing.filechooser.FileNameExtensionFilter
-
 
 import de.thm.icampus.mdd.Builder.EJSLModelFunctionWrapper
 
+import scala.util.{Failure, Success, Try}
+
 object Main extends App {
+
+  val arguments = resolveArguments(args.toList)
+  validateArgumentDependencies(arguments)
+
+  val noGui = arguments.contains(NoGUI)
+
   try {
-    val manifestPath = chooseXMLManifest
-    if (manifestPath.isDefined) {
-      val eJSLModel = Builder.build(manifestPath.get)
 
-      val fileName = s"${manifestPath.get.getParent}/${eJSLModel.name}.eJSL"
-
-      new PrintWriter(fileName.toString) {
-        write(eJSLModel.asText); close()
-      }
-
-      JOptionPane.showMessageDialog(null, "Model Saved in: " + fileName)
+    val manifestPath : Path = if(noGui) {
+      arguments.filter(_.isInstanceOf[ManifestPath]).last.asInstanceOf[ManifestPath].path
+    } else {
+      chooseXMLManifestWithGUI.get
+      //TODO is Option
     }
+
+    println(manifestPath)
+
+    val eJSLModel = Builder.build(manifestPath)
+
+    val outputPath : Path = if(arguments.exists(_.isInstanceOf[OutputPath])) {
+      arguments.filter(_.isInstanceOf[OutputPath]).last.asInstanceOf[OutputPath].path
+    } else {
+      Paths.get(manifestPath.getParent.toAbsolutePath.toString + File.separator + eJSLModel.name + ".eJSL")
+    }
+
+    new PrintWriter(outputPath.toString) {
+      write(eJSLModel.asText); close()
+    }
+
+    if(!noGui){
+      JOptionPane.showMessageDialog(null, "Model Saved in: " + outputPath)
+    }
+
+    println("Model Saved in: " + outputPath)
+
   } catch {
-    case e: Exception ⇒ JOptionPane.showMessageDialog(null, e.getMessage, "Error", JOptionPane.ERROR_MESSAGE)
+    case e: Exception ⇒ {
+      e.printStackTrace()
+      if(!noGui) {
+        JOptionPane.showMessageDialog(null, e.getMessage, "Error", JOptionPane.ERROR_MESSAGE)
+      }
+    }
   }
 
 
@@ -32,7 +60,7 @@ object Main extends App {
    *
    * @return The path to the choosen file. None if Dialog was aborted.
    */
-  def chooseXMLManifest: Option[Path] = {
+  def chooseXMLManifestWithGUI: Option[Path] = {
     val fc = new JFileChooser()
     fc.addChoosableFileFilter(new FileNameExtensionFilter("XML File", "xml"))
 
@@ -42,6 +70,69 @@ object Main extends App {
       Option(fc.getSelectedFile.toPath)
     else
       None
+  }
+
+  sealed abstract class ConsoleArgument
+  case object NoGUI extends ConsoleArgument
+  case class OutputPath(path: Path) extends ConsoleArgument
+  case class ManifestPath(path: Path) extends ConsoleArgument
+
+
+  /**
+    * This method arguments from a list of strings
+    *
+    * @param input list of strings to parse
+    * @return all parsed arguments from the given list
+    */
+  def resolveArguments(input: List[String], arguments: List[ConsoleArgument] = List()) : List[ConsoleArgument] = input match {
+    case Nil => arguments
+    case ("-o" | "-output") :: tail => if(tail.isEmpty) {
+      println("Undefined output path")
+      sys.exit(1)
+    } else {
+      val pathString = tail(0)
+      Try(Paths.get(pathString)) match {
+        case Success(path) => resolveArguments(tail.drop(1), arguments :+ OutputPath(path))
+        case Failure(ex) =>
+          println("Invalid output path")
+          sys.exit(1)
+      }
+    }
+    case ("-m" | "-manifest") :: tail => if(tail.isEmpty) {
+      println("Undefined manifest path")
+      sys.exit(1)
+    } else {
+      val pathString = tail(0)
+      Try(Paths.get(pathString)) match {
+        case Success(path) =>
+          val manifestFile = new File(pathString)
+          if(manifestFile.exists() && (manifestFile.isDirectory | !manifestFile.canRead)) {
+            println("Invalid manifest file")
+            sys.exit(1)
+          }
+          resolveArguments(tail.drop(1), arguments :+ ManifestPath(path))
+        case Failure(ex) =>
+          println("Invalid manifest path")
+          sys.exit(1)
+      }
+    }
+    case ("-ng" | "-no-gui") :: tail => resolveArguments(tail, arguments :+ NoGUI)
+    case argument :: tail =>
+      println("Unknown argument " + argument)
+      sys.exit(1)
+  }
+
+
+  /**
+    * Validates the dependencies between arguments
+    *
+    * @param arguments all arguments to validate
+    */
+  def validateArgumentDependencies(arguments: List[ConsoleArgument]) = {
+    if(arguments.contains(NoGUI) && !arguments.exists(_.isInstanceOf[ManifestPath])) {
+      println("Undefined manifest")
+      sys.exit(1)
+    }
   }
 
 }
