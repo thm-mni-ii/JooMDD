0b8d9fb09cd8112a484004b7acc42cbd77d4e048
Dennis Priefer <dennis.priefer@mni.thm.de>
2017-05-31 19:50:13 +0200

    changed web editor style

--- development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/lib/editorhandler.js
+++ development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/lib/editorhandler.js
@@ -6,12 +6,15 @@ define('editorhandler',[ 'jquery',"jstree", "cookie","webjars/ace/1.2.0/src/ace"
 		var t = xtext_ace.createEditor({
 				baseUrl: baseUrl ,
 				syntaxDefinition: "xtext-resources/generated/mode-eJSL",
-				theme: "ace/theme/twilight",
+				theme: "ace/theme/github",
 				resourceId: username +"/src/" + resourceID
 				
 			});
 		 $("#modelname").text(resourceID)
-
+		 jQuery("#xtext-editor")[0].env.editor.setOptions({
+			 fontSize: "14px"
+         });
 	 };
+	 
 	 return exports;
 })
