41d706187fed03244da43fad6ef16a1838093de7
Dennis Priefer <dennis.priefer@mni.thm.de>
2017-05-31 15:33:40 +0200

    Removed "^" from model name in jext2eJSL

--- development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/model/EJSLModel.scala
+++ development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/model/EJSLModel.scala
@@ -28,7 +28,7 @@ object EJSLModel {
 
     params.foreach(param ⇒ datatypes = datatypes + param.htmltype)
 
-    new EJSLModel(name, datatypes, pages, entities, params, paramGroups, extensions)
+    new EJSLModel(name.drop(1), datatypes, pages, entities, params, paramGroups, extensions)
   }
 
   private def mergePages(a: Set[Page], b: Set[Page]): Set[Page] = {
