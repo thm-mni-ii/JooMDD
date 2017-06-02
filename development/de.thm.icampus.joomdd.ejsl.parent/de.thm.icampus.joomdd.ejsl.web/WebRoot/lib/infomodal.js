7fbb35bbbda45e073abe234b8bbb8bdd3366ee00
Wolf Rost <wolf.normann.gordian.rost@mni.thm.de>
2017-06-02 21:29:50 +0200

    Fixed a problem using tomcat8

--- development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/lib/infomodal.js
+++ development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/lib/infomodal.js
@@ -10,5 +10,16 @@ define('infomodal',[ 'jquery',"jstree", "cookie"], function(jQuery,jstree,Cookie
 	 exports.closeloadmodal = function(text){
 			$("#loadModal").css("display","none");
 		 };
+		 
+	// Get the modal
+	var modal = document.getElementById('infoModal');
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+		 
 	 return exports;
 })
