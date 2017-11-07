8cd57adb75286cefa6522232f731ca6a0d2c362b
Wolf Rost <wolf.normann.gordian.rost@mni.thm.de>
2017-11-07 19:38:15 +0100

    It is possible to load models now. Changed tree reloading after the initial editor load.

--- development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/js/editorhandler.js
+++ development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/js/editorhandler.js
@@ -1,5 +1,6 @@
 define('editorhandler',[ 'jquery',"jstree", "cookie","ace/ace","xtext/xtext-ace","treeloader"], function(jQuery,jstree,Cookies,ace,xtext_ace,treeloader) {
 	 var exports = {};
+	 var editor = null;
 	 exports.loadEditor = function(resourceID){
 		 var baseUrl = window.location.pathname;
 		 var fileIndex = baseUrl.indexOf("index_wolf.html");
@@ -8,7 +9,12 @@ define('editorhandler',[ 'jquery',"jstree", "cookie","ace/ace","xtext/xtext-ace"
 	    	baseUrl = baseUrl.slice(0, fileIndex);
 	    }		 
 
-		var editor = xtext_ace.createEditor({
+		if (editor != null)
+		{
+			editor.destroy();
+		}
+
+		editor = xtext_ace.createEditor({
 				baseUrl: baseUrl ,
 				syntaxDefinition: "xtext-resources/generated/mode-eJSL",
 				theme: "ace/theme/github",
