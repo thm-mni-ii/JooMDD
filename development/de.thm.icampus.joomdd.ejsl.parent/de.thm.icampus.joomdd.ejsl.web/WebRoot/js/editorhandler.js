fe8d93df1bc92f9c31c46a2323b9b060661c9e80
Dennis Priefer <dennis.priefer@mni.thm.de>
2017-11-17 16:04:55 +0100

    Removed Style.css, fixed height of the editor and the folder tree, and fixed some minor style issues in the navbar.

--- development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/js/editorhandler.js
+++ development/de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/js/editorhandler.js
@@ -1,45 +1,45 @@
 define('editorhandler',[ 'jquery',"jstree","ace/ace","xtext/xtext-ace","treeloader"], function(jQuery,jstree,ace,xtext_ace,treeloader) {
 	 var exports = {};
 	 var editor = null;
-	 exports.loadEditor = function(resourceID){
+	 exports.loadEditor = function(resourceID) {
 		var baseUrl = window.location.pathname;
 		var fileIndex = baseUrl.indexOf("editor.html");
-	    if (fileIndex > 0)
-	    {
+	    if (fileIndex > 0) {
 	    	baseUrl = baseUrl.slice(0, fileIndex);
-	    }		 
-
-		if (editor != null)
-		{
+	    }
+	    
+	    if (editor != null) {
 			editor.destroy();
 		}
 
 		editor = xtext_ace.createEditor({
-				baseUrl: baseUrl ,
-				syntaxDefinition: "xtext-resources/generated/mode-eJSL",
-				theme: "ace/theme/github",
-				resourceId: "/src/" + resourceID
-			});
-		 $("#modelname").text(resourceID)
-		 editor.setOptions({
-			 fontSize: "14px"
-         });
+			baseUrl: baseUrl ,
+			syntaxDefinition: "xtext-resources/generated/mode-eJSL",
+			theme: "ace/theme/github",
+			resourceId: "/src/" + resourceID
+		});
+		
+		$("#modelname").text(resourceID)
+		editor.setOptions({
+			fontSize: "14px",
+			minLines: 25
+        });
          
-        var afterEditorCreation = function(){
-        	 var jstree = $('#folder_tree').jstree(true);
+        var afterEditorCreation = function() {
+        	var jstree = $('#folder_tree').jstree(true);
         	 
-        	 if(jstree)
-        	 {
-        	 	treeloader.reload();
-        	 }
-        	 else
-        	 {
-        	 	treeloader.writeTree();
-        	 }
-			 editor.xtextServices.successListeners = editor.xtextServices.successListeners.filter(function(obj) {
-			 											return obj.name !== "afterEditorCreation";
-			 										});
+        	if(jstree) {
+        		treeloader.reload();
+        	}
+        	else
+        	{
+        		treeloader.writeTree();
+        	}
+			editor.xtextServices.successListeners = editor.xtextServices.successListeners.filter(function(obj) {
+				return obj.name !== "afterEditorCreation";
+			});
 		};
+		
 		editor.xtextServices.successListeners.push(afterEditorCreation);
 		
         return editor;
