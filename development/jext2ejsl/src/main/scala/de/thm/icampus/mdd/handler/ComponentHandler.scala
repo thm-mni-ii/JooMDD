457075c1b44e1805072abfc9898d2058d746e044
Dieudonne Timma <dieudonne.timma.meyatchie@mni.thm.de>
2018-08-22 20:23:32 +0200

    Add dbqquery parser

--- development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/handler/ComponentHandler.scala
+++ development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/handler/ComponentHandler.scala
@@ -13,6 +13,7 @@ import scala.collection.JavaConversions._
 import de.thm.icampus.mdd.model.{Language, Manifest}
 import de.thm.icampus.mdd.model.extensions._
 import de.thm.mni.ii.phpparser.ast.Expressions.{MemberCallPropertyAcc, MemberCallStaticAcc, SimpleAssignmentExp}
+import de.thm.mni.ii.phpparser.ast.Expressions.SimpleNameVar
 import de.thm.mni.ii.phpparser.ast.Statements.ExpressionStmnt
 
 import scala.io.{Codec, Source}
@@ -101,37 +102,126 @@ object ComponentHandler extends Handler {
                 }).toString
                 parentName match {
                  case "ListModel" | "JModelList" =>{
+                  val getListItemFunkDT  = DetailsPageHandler.searchMethod("getListQuery",bodyClass)
+                   if(getListItemFunkDT != null){
+                     val allStmtsItem = getListItemFunkDT.body match {  case Some(e) => e
+                     }
+                     var dbstatement = DetailsPageHandler.searchStatmentAfterMember("this","getDbo",allStmtsItem.stmnts,true)
 
-                  }
-                   case "FormModel" | "JModelAdmin"=>{
+                     if(dbstatement != null){
+                       val dbexp = dbstatement._1.exp.asInstanceOf[SimpleAssignmentExp]
+                       val dbVarName = dbexp.variable.asInstanceOf[SimpleNameVar].name.name
+                       val fd = DetailsPageHandler.searchStatmentAfterVariable(dbVarName,dbstatement._2,true)
+                       if(fd != null){
+                         val expm = fd._1.exp.asInstanceOf[SimpleAssignmentExp]
+                         val queryVar = DetailsPageHandler.readVaraibleName(expm.variable)
+                         val from = DetailsPageHandler.searchStatmentAfterMember(queryVar,"from",fd._2,true)
+                         println(from)
+                       }
 
+                       println(fd._1)
+
+                     }
+                   }
                   }
-                   case "ItemModel" | "JModelAdmin" =>{
+
+                   case "ItemModel" | "JModelAdmin" | "FormModel" | "JModelItem"=>{
                      var tableName = ""
                       val getItemFunk = DetailsPageHandler.searchMethod("getItem",bodyClass)
                      if(getItemFunk != null){
 
                        val allStmts = getItemFunk.body match {  case Some(e) => e
                        }
-                       val statement = DetailsPageHandler.searchStatment("getTable",allStmts.stmnts,true)
+                       val statement = DetailsPageHandler.searchStatmentAfterMember("this","getTable",allStmts.stmnts,true)
+
                        if(statement != null){
-                          val assStat = statement.asInstanceOf[ExpressionStmnt]
-                         val expm =assStat.exp.asInstanceOf[SimpleAssignmentExp]
+                         val expm = statement._1.exp.asInstanceOf[SimpleAssignmentExp]
                          val call = expm.exp.asInstanceOf[MemberCallPropertyAcc]
                          if(call.args.size != 0){
                            tableName = DetailsPageHandler.readMemberCallStaticAccAttr(call.from,call.member,call.args,getItemFunk,"this","getTable",0)
                          }
-
                        }
-
                      }
-                    if(tableName ==""){
+                      if (tableName =="" || tableName == null){
+                       val getSave = DetailsPageHandler.searchMethod("save",bodyClass)
+                       if(getSave != null){
+
+                         val allStmtsSave = getSave.body match {  case Some(e) => e
+                         }
+                         val statement = DetailsPageHandler.searchStatmentAfterMember("this","getTable",allStmtsSave.stmnts,true)
+                         if(statement != null){
+                           val expm =statement._1.exp.asInstanceOf[SimpleAssignmentExp]
+                           val call = expm.exp.asInstanceOf[MemberCallPropertyAcc]
+                           if(call.args.size != 0){
+                             tableName = DetailsPageHandler.readMemberCallStaticAccAttr(call.from,call.member,call.args,getSave,"this","getTable",0)
+                           }
+                         }
+                       }
+                     }
+                      if (tableName =="" || tableName == null){
                       val getTableFunc = DetailsPageHandler.searchMethod("getTable",bodyClass)
                      if(getTableFunc != null){
                        tableName = DetailsPageHandler.searchTableInMeth(getTableFunc)
                        println(tableName)
                      }
                     }
+                      if(tableName =="" || tableName == null){
+                       var tempTablePAth = backendPath +"tables" + modelFilename
+                       if(Files.exists(tempTablePAth)){
+                         tableName = pageName
+                       }
+                     }
+                     var dbName = ""
+                     if(tableName != "" && tableName != null){
+                       val tablePath = backendPath +"tables" + (tableName.toLowerCase + ".php")
+                       if(Files.exists(tablePath)){
+                      val tableContentText = Source.fromFile(tablePath).mkString
+                       val tableContentParsed = PHPParser.parse(tableContentText)
+                       val tableClass = DetailsPageHandler.searchClass(tableContentParsed)
+                       val tableBodyClass = tableClass.body
+                         val consMeth = DetailsPageHandler.searchMethod("__construct",tableBodyClass)
+                         if(consMeth != null){
+                           val consMethStmtsSave = consMeth.body match {  case Some(e) => e
+                           }
+                           val statement = DetailsPageHandler.searchStatmentAfterMember("parent","__construct",consMethStmtsSave.stmnts,true)
+
+                           if(statement != null && statement._1.exp.isInstanceOf[ MemberCallStaticAcc]){
+                             val call = statement._1.exp.asInstanceOf[MemberCallStaticAcc]
+                             if(call.args.size != 0){
+                               dbName  = DetailsPageHandler.readMemberCallStaticAccAttr(call.from,call.member,call.args,consMeth,"parent","__construct",0)
+                              println(dbName)
+                             }
+                           }
+
+                         }
+
+                       }
+                     }else{
+                       val getItemFunkDT = DetailsPageHandler.searchMethod("getItem",bodyClass)
+                       if(getItemFunkDT != null){
+                         val allStmtsItem = getItemFunk.body match {  case Some(e) => e
+                         }
+                         var dbstatement = DetailsPageHandler.searchStatmentAfterMember("this","getDbo",allStmtsItem.stmnts,true)
+
+                         if(dbstatement != null){
+                           val dbexp = dbstatement._1.exp.asInstanceOf[SimpleAssignmentExp]
+                           val dbVarName = dbexp.variable.asInstanceOf[SimpleNameVar].name.name
+                           val fd = DetailsPageHandler.searchStatmentAfterVariable(dbVarName,dbstatement._2,true)
+                           if(fd != null){
+                             val expm = fd._1.exp.asInstanceOf[SimpleAssignmentExp]
+                             val queryVar = DetailsPageHandler.readVaraibleName(expm.variable)
+                             val from = DetailsPageHandler.searchStatmentAfterMember(queryVar,"from",fd._2,true)
+                             println(from)
+                           }
+
+                          println(fd._1)
+
+                         }
+                       }
+                     }
+
+
+
                    }
                  }
 
--- development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/handler/DetailsPageHandler.scala
+++ development/jext2ejsl/src/main/scala/de/thm/icampus/mdd/handler/DetailsPageHandler.scala
@@ -9,41 +9,229 @@ import de.thm.mni.ii.phpparser.ast.Expressions._
 import de.thm.mni.ii.phpparser.ast.Basic
 
 object DetailsPageHandler {
-  def searchStatment(str: String, statements: Seq[Statement], checkIFstmnt:Boolean): Statement={
-   for(item <-statements){
-       item match {
-         case Statements.TryStmnt(stmnt, catches, fin)=>{
-          return searchStatment(str,stmnt.stmnts,checkIFstmnt)
-         }
-         case Statements.IfStmnt(exp,stmntsIf,elseifs,elseStmnts)=>{
-           if(checkIFstmnt){
-             val tempvarValue = searchStatment(str,stmntsIf,checkIFstmnt)
-             if(tempvarValue == null && elseStmnts != null){
-               elseStmnts match {
-                 case Some(elseItems)=>{
-                   val tempvarValue = searchStatment(str,elseItems,checkIFstmnt)
-                 }
-               }
-             }
-             return tempvarValue
-           }
-         }
-         case Statements.ExpressionStmnt(exp)=>{
-           if(exp.isInstanceOf[SimpleAssignmentExp]){
-             val c = exp.asInstanceOf[SimpleAssignmentExp]
-             c.exp match{
-               case MemberCallPropertyAcc(from,member,args)=>{
-                 if(member.isInstanceOf[NameMember] && (member.asInstanceOf[NameMember]).name.name == str){
-                   return item;
-                 }
-               }
-               case _=>
-             }
-           }
 
-         }
-         case _=>
-       }
+  def searchExpressionAfterFrom(fromName: String, from: Expression): Expression ={
+
+    from match{
+      case v :MemberCallPropertyAcc =>{
+        val g = searchExpressionAfterFrom(fromName,v.from)
+        if(g != null)
+          return g
+
+      }
+      case f :MemberCallStaticAcc =>{
+        val g = searchExpressionAfterFrom(fromName,f.from)
+        if(g != null)
+          return g
+      }
+      case b: SimpleNameVar=>{
+        if(b.name.name == fromName){
+          return from
+        }
+      }
+      case c :QualifiedNameVar =>{
+        if(c.name.name.name == fromName)
+        return from
+      }
+
+      case e: ScopeAccessVar =>{
+        if(e.scope.toString.toLowerCase == fromName)
+        return from
+      }
+      case _=>
+    }
+
+    return null
+  }
+
+  def searchExpressionAfterMember( fromVarName:String,memberName: String, from: Expression): Expression ={
+
+    from match{
+      case v :MemberCallPropertyAcc =>{
+        if((v.member.asInstanceOf[NameMember]).name.name == memberName && readVaraibleName(v.from) == fromVarName){
+          return from
+        }
+        else{searchExpressionAfterFrom(memberName,v.from)}
+
+      }
+      case f :MemberCallStaticAcc =>{
+        if((f.member.asInstanceOf[NameMember]).name.name == memberName&& readVaraibleName(f.from) == fromVarName ){
+          return from
+        }
+        else{searchExpressionAfterFrom(memberName,f.from)}
+
+      }
+      case b: SimpleNameVar=>{
+
+          return null
+      }
+
+      case c :QualifiedNameVar =>{
+
+          return null
+      }
+
+      case e: ScopeAccessVar =>{
+
+          return null
+      }
+      case _=>
+    }
+
+    return null
+  }
+
+  def searchStatmentAfterVariable(fromName: String, statements: Seq[Statement], checkIFstmnt:Boolean): (ExpressionStmnt, Seq[Statement])={
+    for(item <-statements){
+      if(item.isInstanceOf[CompoundStmnt]){
+        val r = searchStatmentAfterVariable(fromName,(item.asInstanceOf[CompoundStmnt]).stmnts,checkIFstmnt)
+        if(r != null && r._1 != null)
+          return r
+      }else{
+        item match {
+          case Statements.TryStmnt(stmnt, catches, fin)=>{
+
+            var result =  searchStatmentAfterVariable(fromName,stmnt.stmnts,checkIFstmnt)
+            if(result!= null && result._1 != null)
+              return result
+
+          }
+          case Statements.IfStmnt(exp,stmntsIf,elseifs,elseStmnts)=>{
+            if(checkIFstmnt){
+              val tempvarValue = searchStatmentAfterVariable(fromName,stmntsIf,checkIFstmnt)
+              if(tempvarValue == null && elseStmnts != null && !elseStmnts.isEmpty){
+                elseStmnts match {
+                  case Some(elseItems)=>{
+                    val tempvarValue = searchStatmentAfterVariable(fromName,elseItems,checkIFstmnt)
+                  }
+                }
+              }
+              if(tempvarValue != null && tempvarValue._1 != null)
+                return  tempvarValue
+            }
+          }
+          case Statements.ExpressionStmnt(exp)=>{
+            exp match {
+              case c : SimpleAssignmentExp =>{
+                c.exp match{
+                  case MemberCallPropertyAcc(from,member,args)=>{
+                    if(from.isInstanceOf[SimpleNameVar] && (from.asInstanceOf[SimpleNameVar]).name.name == fromName){
+                      return (item.asInstanceOf[ExpressionStmnt] , statements);
+                    }else{
+                      var j = searchExpressionAfterFrom(fromName,from)
+                      if(j != null)
+                        return (item.asInstanceOf[ExpressionStmnt],statements);
+                    }
+                  }
+                  case _=>
+                }
+              }
+              case v :MemberCallPropertyAcc =>{
+                if(v.from.isInstanceOf[SimpleNameVar] && (v.from.asInstanceOf[SimpleNameVar]).name.name == fromName){
+                  return (item.asInstanceOf[ExpressionStmnt],statements);
+                }else{
+
+                  var j = searchExpressionAfterFrom(fromName,v.from)
+                  if(j != null && j != "")
+                    return (item.asInstanceOf[ExpressionStmnt],statements);
+
+                }
+              }
+              case f :MemberCallStaticAcc =>{
+                if(f.from.isInstanceOf[SimpleNameVar] && (f.from.asInstanceOf[SimpleNameVar]).name.name == fromName){
+                  return (item.asInstanceOf[ExpressionStmnt],statements);
+                }else{
+                  var j = searchExpressionAfterFrom(fromName,f.from)
+                  if(j != null && j != "")
+                    return (item.asInstanceOf[ExpressionStmnt],statements);
+                }
+              }
+              case _=>
+
+            }
+
+          }
+          case _=>
+        }
+      }
+
+
+    }
+
+    return null
+  }
+
+  def searchStatmentAfterMember(fromVarName:String,memberName: String, statements: Seq[Statement], checkIFstmnt:Boolean): (ExpressionStmnt, Seq[Statement])={
+   for(item <-statements){
+      if(item.isInstanceOf[CompoundStmnt]){
+       val r = searchStatmentAfterMember(fromVarName,memberName,(item.asInstanceOf[CompoundStmnt]).stmnts,checkIFstmnt)
+        if(r != null && r._1 != null)
+          return r
+      }else{
+        item match {
+          case Statements.TryStmnt(stmnt, catches, fin)=>{
+
+            var result =  searchStatmentAfterMember(fromVarName,memberName,stmnt.stmnts,checkIFstmnt)
+            if(result!= null && result._1 != null)
+              return result
+
+          }
+          case Statements.IfStmnt(exp,stmntsIf,elseifs,elseStmnts)=>{
+            if(checkIFstmnt){
+              val tempvarValue = searchStatmentAfterMember(fromVarName,memberName,stmntsIf,checkIFstmnt)
+              if(tempvarValue == null && elseStmnts != null && !elseStmnts.isEmpty){
+                elseStmnts match {
+                  case Some(elseItems)=>{
+                    val tempvarValue = searchStatmentAfterMember(fromVarName,memberName,elseItems,checkIFstmnt)
+                  }
+                }
+              }
+              if(tempvarValue != null && tempvarValue._1 != null)
+                return  tempvarValue
+            }
+          }
+          case Statements.ExpressionStmnt(exp)=>{
+            exp match {
+              case c : SimpleAssignmentExp =>{
+                c.exp match{
+                  case MemberCallPropertyAcc(from,member,args)=>{
+                    if(!from.isInstanceOf[SimpleNameVar]){
+                      val h = searchExpressionAfterMember(fromVarName,memberName,from)
+                      if(h != null)
+                        return (item.asInstanceOf[ExpressionStmnt] , statements);
+                    }
+                    if(member.isInstanceOf[NameMember] && (member.asInstanceOf[NameMember]).name.name == memberName && readVaraibleName(from) == fromVarName){
+                      return (item.asInstanceOf[ExpressionStmnt] , statements);
+                    }
+                  }
+                  case _=>
+                }
+              }
+              case v :MemberCallPropertyAcc =>{
+                if(!v.from.isInstanceOf[SimpleNameVar]){
+                  val h = searchExpressionAfterMember(fromVarName,memberName,v.from)
+                  if(h != null)
+                    return (item.asInstanceOf[ExpressionStmnt] , statements);
+                }
+                if(v.member.isInstanceOf[NameMember] && (v.member.asInstanceOf[NameMember]).name.name == memberName && readVaraibleName(v.from) == fromVarName){
+                  return (item.asInstanceOf[ExpressionStmnt],statements);
+                }
+              }
+
+              case f :MemberCallStaticAcc =>{
+                if(f.member.isInstanceOf[NameMember] && (f.member.asInstanceOf[NameMember]).name.name == memberName&& readVaraibleName(f.from) == fromVarName){
+                  return (item.asInstanceOf[ExpressionStmnt],statements);
+                }
+              }
+              case _=>
+
+            }
+
+          }
+          case _=>
+        }
+      }
+
 
      }
 
@@ -169,14 +357,8 @@ object DetailsPageHandler {
   def readMemberCallStaticAccAttr(from:Variable,member:MemberName,args:scala.Seq[Expressions.ArgumentExpression],geTabelMeth:MethodDecl,fromName:String,memberName:String,argsindex:Integer): String ={
     val allStmts = geTabelMeth.body match {  case Some(e) => e
     }
-    var className = ""
-    if(from.isInstanceOf[QualifiedNameVar]){
-      className = (from.asInstanceOf[QualifiedNameVar]).name.name.name
-    }
 
-    else{
-      className = (from.asInstanceOf[SimpleNameVar]).name.name
-    }
+    var className = readVaraibleName(from)
       val memberVal = (member.asInstanceOf[NameMember]).name.name
       if(className == fromName && memberVal ==memberName){
         val typeTable = args(argsindex).exp;
@@ -213,6 +395,22 @@ object DetailsPageHandler {
       case _=>
         return null
     }
+  }
+  def readVaraibleName(exp:Expression):String={
+
+    exp match{
+      case c :QualifiedNameVar =>{
+        return c.name.name.name
+      }
+      case d :SimpleNameVar=>{
+        return d.name.name
+      }
+      case e: ScopeAccessVar =>{
+        return e.scope.toString.toLowerCase
+      }
+      case _=>
+    }
+    return ""
   }
 
 }
