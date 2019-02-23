5e5e194d7d8db049cda7fe2d83d58cfef8c464da
Lukas Kimpel <mail@lukaskimpel.com>
2019-02-23 18:02:11 +0100

    Fix jQuery dependency error, fix icon

--- de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/js/interactive-tour.js
+++ de.thm.icampus.joomdd.ejsl.parent/de.thm.icampus.joomdd.ejsl.web/WebRoot/js/interactive-tour.js
@@ -1,171 +1,177 @@
-(function ($) {
-    'use strict';
+require(["jquery"], function($) {
+    (function ($) {
+        'use strict';
 
-    var dimmedNodes = [];
+        var dimmedNodes = [];
 
-    $.fn.dimBackground = function (options, callback, intel) {
-        var params = parseParams(options, callback, intel);
-        options  = params.options;
-        callback = params.callback;
-        intel = params.intel
+        $.fn.dimBackground = function (options, callback, intel) {
+            var params = parseParams(options, callback, intel);
+            options = params.options;
+            callback = params.callback;
+            intel = params.intel
 
-        options = $.extend({}, $.fn.dimBackground.defaults, options);
+            options = $.extend({}, $.fn.dimBackground.defaults, options);
 
-        var $curtain = $('<div class="dimbackground-curtain"></div>');
-        $curtain.css({
-            position:   'fixed',
-            left:     0,
-            top:    0,
-            width:    '100%',
-            height:   '100%',
-            background: 'black',
-            opacity:  0,
-            zIndex:   options.curtainZIndex
-        });
-        $('body').append($curtain);
+            var $curtain = $('<div class="dimbackground-curtain"></div>');
+            $curtain.css({
+                position: 'fixed',
+                left: 0,
+                top: 0,
+                width: '100%',
+                height: '100%',
+                background: 'black',
+                opacity: 0,
+                zIndex: options.curtainZIndex
+            });
+            $('body').append($curtain);
 
-        this.each(function (){
-            var $this = $(this);
-            var opts = $.meta ? $.extend( {}, options, $this.data() ) : options;
+            this.each(function () {
+                var $this = $(this);
+                var opts = $.meta ? $.extend({}, options, $this.data()) : options;
 
-            this._$curtain = $curtain;
-            this._originalPosition = $this.css('position').toLowerCase();
-            if (this._originalPosition != "absolute" && this._originalPosition != "fixed") {
-                $this.css('position', 'relative');
-            }
+                this._$curtain = $curtain;
+                this._originalPosition = $this.css('position').toLowerCase();
+                if (this._originalPosition != "absolute" && this._originalPosition != "fixed") {
+                    $this.css('position', 'relative');
+                }
 
-            this._originalZIndex = $this.css('z-index');
-            if (this._originalZIndex == "auto" || this._originalZIndex <= opts.curtainZIndex) {
-                $this.css('z-index', opts.curtainZIndex+1);
-            }
+                this._originalZIndex = $this.css('z-index');
+                if (this._originalZIndex == "auto" || this._originalZIndex <= opts.curtainZIndex) {
+                    $this.css('z-index', opts.curtainZIndex + 1);
+                }
 
-            $this.popover({
-                content: intel,
-                template: '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+                $this.popover({
+                    content: intel,
+                    template: '<div class="popover" role="tooltip"><div class="arrow"></div><h3 class="popover-header"></h3><div class="popover-body"></div></div>'
+                });
+
+                $this.popover('show');
             });
 
-            $this.popover('show');
-        });
+            $curtain.stop().animate({opacity: options.darkness}, options.fadeInDuration, callback);
+            dimmedNodes.push({$curtain: $curtain, $nodes: this});
+            return this;
+        };
 
-        $curtain.stop().animate({opacity: options.darkness}, options.fadeInDuration, callback);
-        dimmedNodes.push( {$curtain: $curtain, $nodes: this} );
-        return this;
-    };
+        $.fn.undim = function (options, callback) {
+            var params = parseParams(options, callback);
+            options = params.options;
+            callback = params.callback;
+            options = $.extend({}, $.fn.dimBackground.defaults, options);
 
-    $.fn.undim = function (options, callback) {
-        var params = parseParams(options, callback);
-        options  = params.options;
-        callback = params.callback;
-        options = $.extend({}, $.fn.dimBackground.defaults, options);
+            var $curtain;
+            var nodeZIndexMap = [];
+            this.each(function () {
+                var $this = $(this);
+                var opts = $.meta ? $.extend({}, options, $this.data()) : options;
 
-        var $curtain;
-        var nodeZIndexMap = [];
-        this.each(function () {
-            var $this = $(this);
-            var opts = $.meta ? $.extend( {}, options, $this.data() ) : options;
-
-            if (this._$curtain) {
-                if (!$curtain) {
-                    $curtain = this._$curtain;
+                if (this._$curtain) {
+                    if (!$curtain) {
+                        $curtain = this._$curtain;
+                    }
+                    if (typeof this._originalPosition != "undefined") {
+                        nodeZIndexMap.push([this, this._originalPosition, this._originalZIndex]);
+                    }
+                    this._$curtain = undefined;
+                    this._originalPosition = undefined;
+                    this._originalZIndex = undefined;
                 }
-                if (typeof this._originalPosition != "undefined") {
-                    nodeZIndexMap.push( [this, this._originalPosition, this._originalZIndex] );
-                }
-                this._$curtain = undefined;
-                this._originalPosition = undefined;
-                this._originalZIndex = undefined;
-            }
-        });
 
-        if ($curtain) {
-            $curtain.animate({opacity: 0}, options.fadeOutDuration, function () {
-                for (var i=0; i<nodeZIndexMap.length; i++) {
-                    var node = nodeZIndexMap[i][0],
-                        position = nodeZIndexMap[i][1],
-                        zIndex = nodeZIndexMap[i][2];
-                    $(node).css({
-                        position: position,
-                        zIndex: zIndex
-                    });
-                }
-                $curtain.remove();
-                callback();
+                $this.popover('hide');
             });
 
-            var match;
-            for (var i=0; i<dimmedNodes.length; i++) {
-                var entry = dimmedNodes[i];
-                if (entry.$curtain == $curtain) {
-                    match = i;
-                    break;
+            if ($curtain) {
+                $curtain.animate({opacity: 0}, options.fadeOutDuration, function () {
+                    for (var i = 0; i < nodeZIndexMap.length; i++) {
+                        var node = nodeZIndexMap[i][0],
+                            position = nodeZIndexMap[i][1],
+                            zIndex = nodeZIndexMap[i][2];
+                        $(node).css({
+                            position: position,
+                            zIndex: zIndex
+                        });
+                    }
+                    $curtain.remove();
+                    callback();
+                });
+
+                var match;
+                for (var i = 0; i < dimmedNodes.length; i++) {
+                    var entry = dimmedNodes[i];
+                    if (entry.$curtain == $curtain) {
+                        match = i;
+                        break;
+                    }
+                }
+                if (match) {
+                    dimmedNodes = dimmedNodes.slice(0, i).concat(dimmedNodes.slice(i + 1));
                 }
             }
-            if (match) {
-                dimmedNodes = dimmedNodes.slice(0, i).concat( dimmedNodes.slice(i+1) );
+            return this;
+        };
+
+        $.undim = function (options, callback) {
+            var params = parseParams(options, callback);
+            options = params.options;
+            callback = params.callback;
+            options = $.extend({}, $.fn.dimBackground.defaults, options);
+
+            var _dimmedNodes = dimmedNodes.slice();
+
+            var completed = 0, total = _dimmedNodes.length;
+            for (var i = 0; i < dimmedNodes.length; i++) {
+                _dimmedNodes[i].$nodes.undim(options, done);
             }
-        }
-        return this;
-    };
 
-    $.undim = function (options, callback) {
-        var params = parseParams(options, callback);
-        options = params.options;
-        callback = params.callback;
-        options = $.extend({}, $.fn.dimBackground.defaults, options);
-
-        var _dimmedNodes = dimmedNodes.slice();
-
-        var completed = 0, total = _dimmedNodes.length;
-        for (var i=0; i<dimmedNodes.length; i++) {
-            _dimmedNodes[i].$nodes.undim(options,done);
-        }
-
-        if (total === 0) {
-            callback();
-        }
-
-        function done () {
-            completed++;
-            if (completed == total) {
+            if (total === 0) {
                 callback();
             }
+
+            function done() {
+                completed++;
+                if (completed == total) {
+                    callback();
+                }
+            }
+        };
+
+        $.fn.dimBackground.defaults = {
+            darkness: 0.75,
+            fadeInDuration: 250,
+            fadeOutDuration: 250,
+            curtainZIndex: 999
+        };
+
+        function parseParams(options, callback, intel) {
+            if (typeof options == "function") {
+                callback = options;
+                intel = callback;
+                options = {};
+            }
+            if (typeof options != "object") {
+                options = {};
+            }
+            if (typeof callback != "function") {
+                callback = function () {
+                };
+            }
+
+            return {options, callback, intel};
         }
-    };
+    }(jQuery));
 
-    $.fn.dimBackground.defaults = {
-        darkness    : 0.75,
-        fadeInDuration  : 250,
-        fadeOutDuration : 250,
-        curtainZIndex   : 999
-    };
+    $(() => {
 
-    function parseParams (options, callback, intel) {
-        if (typeof options == "function") {
-            callback = options;
-            intel = callback;
-            options = {};
-        }
-        if (typeof options != "object") {
-            options = {};
-        }
-        if (typeof callback != "function") {
-            callback = function () {};
-        }
+        setTimeout(() => {
+            // Dim the background except the selected element(s)
+            $('#saveModel').dimBackground({}, () => {
+            }, 'Das ist der Speichern Button');
+        }, 1000);
 
-        return {options, callback, intel};
-    }
-}( jQuery ));
+        setTimeout(() => {
+            // Un-dim all the elements, reset the selected element's values for zIndex and position
+            $('#saveModel').undim();
+        }, 5000);
 
-$(() => {
-
-    setTimeout(() => {
-        // Dim the background except the selected element(s)
-        $('#saveModel').dimBackground({}, () => {}, 'Das ist der Speichern Button');
-    }, 1000);
-
-    setTimeout(() => {
-        // Un-dim all the elements, reset the selected element's values for zIndex and position
-        $('#saveModel').undim();
-    }, 5000);
-
+    });
 });
